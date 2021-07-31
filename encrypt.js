af702b14c89b2bc165bef2ad9bf4a35d1e7eaa20
indhuJothi <indhu.palanivel@aspiresys.com>
2021-07-31 17:55:42 +0530

    commit

--- encrypt.js
+++ encrypt.js
@@ -1,6 +1,18 @@
 
 const crypto = require ("crypto");
-const cipher =crypto.createCipher('aes192','a password')
-var encrypted=cipher.update(" Hello world","utf-8",'hex');
-encrypted+=cipher.final('hex')
-console.log(encrypted)
+const text = 'Hello World'
+const algo = 'aes-192-cbc'
+const key = crypto.scryptSync(text,'salt',27)
+const cipher = crypto.createCipher(algo,key)
+let encryption = ' '
+cipher.on('readable',()=>{
+    let data;
+    while(data= cipher.read()){
+        encryption+=data.toString('hex')
+
+    }
+})
+
+cipher.on('end',()=>console.log(encryption))
+// cipher.write('I am writing something here to test')
+cipher.end()
